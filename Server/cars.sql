d7343985924f968598a2b332bc21f0d5ee83ce95
Eddie C <z5021028@zmail.unsw.edu.au>
2019-11-21 14:43:15 +1100

    Update cars.sql

--- Server/cars.sql
+++ Server/cars.sql
@@ -1,17 +1,14 @@
 CREATE TABLE Users (
-user_id int primary key,
+user_id INTEGER PRIMARY KEY AUTOINCREMENT,
 username text,
 password text
 );
-
 CREATE TABLE Admins (
 admin_id int primary key,
 username text
 );
-
-CREATE TABLE apiusage( -- probably will send me the param of time and api type, which is why i'm just using ApiName text as primary key,
-                       -- then we'll return the table as json with all the data inside, probably make the chart/display accordingly with that object...?
-ApiName text primary_key, --not sure if i should change this to API key and make a new table to map API to keys instead
+CREATE TABLE apiusage(  //backup system for api usage if monittoring board doesn't work
+ApiName text primary_key,
 user_id int,
 Time Datetime,
 foreign key(user_id) references users (user_id)
